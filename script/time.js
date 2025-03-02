12f7dc8f0417585352ff1fc688a1e3f1b0a56f0e
aashikur <mdaashikur@gmail.com>
2025-03-02 19:12:57 +0600

    lol Created a Funciton Will Handle All Time issue :D

--- script/time.js
+++ script/time.js
@@ -9,15 +9,21 @@ function Time(x){
         return now.toDateString().slice(4);
     if(x == "NowTime") {
         let ss = now.getSeconds();
+            ss = (ss <10)? "0"+ss : ss;
+
         let mm = now.getMinutes();
+            mm = (mm < 10)? "0"+mm:mm;
+
         let  hh = now.getHours();
         let t = (hh > 12)? 'PM' : 'AM';
+        
             hh %=12;
+            hh = (hh < 10)? "0"+ hh : hh;
 
         
 
 
-        return `${hh}':'${mm}':'${ss}' '${t}`;
+        return `${hh}:${mm}:${ss} ${t}`;
     }
 }
 
