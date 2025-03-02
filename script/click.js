fa6ca2d43030e5cdcbec7740e7f2a18c212ec876
aashikur <mdaashikur@gmail.com>
2025-03-02 21:34:16 +0600

    Dynamic Time Added

--- script/click.js
+++ script/click.js
@@ -5,30 +5,41 @@ const TaskTotal = document.getElementById('TaskTotal');
 const History = document.getElementById('History');
 
 
+    
+    function ClickBtn(num){
 
+        btn[num].addEventListener('click',function(){
+            btn[num].setAttribute('disabled',true);
+            console.log("hello");
+            TaskLeft.innerHTML ="0" + ( parseFloat(TaskLeft.innerHTML) - 1);
+            TaskTotal.innerHTML =( parseFloat(TaskTotal.innerHTML) + 1);
 
-console.log(TaskTotal.innerHTML)
-
-    btn[0].addEventListener('click',function(){
-        btn[0].setAttribute('disabled',true);
-        console.log("hello");
-        TaskLeft.innerHTML ="0" + ( parseFloat(TaskLeft.innerHTML) - 1);
-        TaskTotal.innerHTML =( parseFloat(TaskTotal.innerHTML) + 1);
-        
-        
-    }) 
-
-    btn[1].addEventListener('click',function(){
-        btn[1].setAttribute('disabled',true);
-        console.log("hello");
-        TaskLeft.innerHTML ="0" + ( parseFloat(TaskLeft.innerHTML) - 1);
-        TaskTotal.innerHTML = ( parseFloat(TaskTotal.innerHTML) + 1);
-        
         // Add title 
         const p = document.createElement('p');
+
         p.classList.add('px-5', 'py-2', 'rounded-md', 'bg-white', 'mb-5', 'text-text');
-        const x = Time('Day');
+        p.innerHTML = `You have Complete The Task Add Dark Mode at <span class="text-primary">${Time("NowTime")}</span>`;
+
+        
         History.appendChild(p);
+        }) 
+    }
 
-    }) 
 
+    for(let i = 0; i<6 ; i++)   
+        ClickBtn(i);
+    
+
+// DayTime 
+// FullDayTime 
+// NowTime
+
+
+    // btn[0].addEventListener('click',function(){
+    //     btn[0].setAttribute('disabled',true);
+    //     console.log("hello");
+    //     TaskLeft.innerHTML ="0" + ( parseFloat(TaskLeft.innerHTML) - 1);
+    //     TaskTotal.innerHTML =( parseFloat(TaskTotal.innerHTML) + 1);
+        
+        
+    // }) 
